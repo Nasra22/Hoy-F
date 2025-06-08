dcd4fdfbbb365941e11e425151a65f5616604cc9
Nasra22 <67530330+Nasra22@users.noreply.github.com>
2025-06-08 03:24:43 +0300

    Update route.ts

--- app/api/property-service/route.ts
+++ app/api/property-service/route.ts
@@ -1,19 +1,19 @@
-import { Property } from "@/lib/agent-service"
+import { Property } from "@/lib/property-service"
 import app from "@/lib/firebase-service"
-import { getAuth } from "firebase/auth"
-import { addDoc, collection, doc, getDocs, getFirestore, setDoc } from "firebase/firestore"
+import { addDoc, collection, deleteDoc, doc, getDocs, getFirestore, serverTimestamp, setDoc } from "firebase/firestore"
 
-
-const auth = getAuth(app)
 const db = getFirestore(app)
 
 export async function addProperty(property: Property) {
+    console.log(property)
+
     await addDoc(collection(db, "Properties" ), {
         title: property.title,
         price: property.price,
         type: property.type,
         bedrooms: property.bedrooms,
         bathrooms: property.bathrooms,
+        owner: property.owner,
         size: property.size,
         status: property.status,
         description: property.description,
@@ -22,11 +22,17 @@ export async function addProperty(property: Property) {
         state: property.state,
         amenities: property.amenities,
         isPublished: property.isPublished,
-        media: property.media
+        isVerified: "pending",
+        media: property.media,
+        timeStamp: serverTimestamp()
     })
 }
 
-export async function getProperty() {
+export async function getProperties() {
     const snapShot = await getDocs(collection(db, "Properties"))
     return snapShot
+}
+
+export async function deleteProperty(doc_id: string) {
+    await deleteDoc(doc(db, "Properties", doc_id))
 }
